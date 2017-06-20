3c6e4a5c6ccc14832a59ede222167c066bd29b05
Paul Arizpe <paul@iterando.mx>
2017-06-20 02:33:43 -0500

    Started

---
+++ Vision/Models.go
@@ -0,0 +1,16 @@
+package Vision
+
+// GetAvailableDomainModels get all Models
+func GetAvailableDomainModels() {
+
+}
+
+// UploadAndAnalyzeInDomainImage upload Image to Analyze
+func UploadAndAnalyzeInDomainImage() {
+
+}
+
+// AnalyzeInDomainUrl upload Image to Analyze
+func AnalyzeInDomainUrl() {
+
+}
